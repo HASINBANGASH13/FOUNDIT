288807fdd50e25b460f938a501315e11e519a1a3
HASINBANGASH13 <hasinmehdi0@gmail.com>
2026-07-21 18:11:15 +0500

    first commit server.js

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import notFound from "./middleware/notFoundMiddleware.js";
+import errorHandler from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test Route
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to FindIt API 🚀",
+    });
+});
+
+app.use("/api/auth", authRoutes);
+// 404 Middleware
+app.use(notFound);
+
+// Error Middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
